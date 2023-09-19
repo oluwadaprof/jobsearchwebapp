d199e68c1a14276d9e05985fc0e7ef17331f05b3
Oluwadaprof <adeekotobiloba8@gmail.com>
2023-09-19 19:58:56 +0100

    Feat: Added Value Section

--- src/components/JobBox/Jobs.jsx
+++ src/components/JobBox/Jobs.jsx
@@ -6,14 +6,14 @@ const Jobs = () => {
     <div>
       <div
         className="jobContainer flex gap-10 justify-center
-    flex-wrap items-center py-10"
+            flex-wrap items-center py-10"
       >
         {JobData.map(({ id, image, title, time, location, desc, company }) => (
           <>
             <div
               key={id}
               className="singleJob group group/item  w-[250px] p-[20px] bg-white rounded-[10px]
-      hover:bg-blueColor shadow-lg shadow-grayIsh-400/700 hover:shadow-lg"
+              hover:bg-blueColor shadow-lg shadow-grayIsh-400/700 hover:shadow-lg"
             >
               <span className="flex justify-between items-center gap-4">
                 <h1 className="text-[16px] font-semibold text-textColor group-hover:text-white ">
@@ -28,7 +28,7 @@ const Jobs = () => {
 
               <p
                 className="text-[13px] text-[#959595] pt-[20px] border-t-[2px]
-            mt-[20px] group-hover:text-white "
+                  mt-[20px] group-hover:text-white "
               >
                 {desc}
               </p>
@@ -42,7 +42,7 @@ const Jobs = () => {
 
               <button
                 className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px]
-         font-semibold text-textColor hover:bg-white group-hover/item:text-textColor group-hover:text-white "
+                  font-semibold text-textColor hover:bg-white group-hover/item:text-textColor "
               >
                 Apply Now
               </button>
--- src/components/ValueBox/Value.jsx
+++ src/components/ValueBox/Value.jsx
@@ -1,11 +1,62 @@
-
+import simple from "../../assets/logo1.png";
+import twitter from "../../assets/logo5.jpg";
+import tiktok from "../../assets/logo6.png";
 
 const Value = () => {
   return (
-    <div>
-      Value
-    </div>
-  )
-}
+    <div className="mb-[4rem] mt-[6rem] ">
+      <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block ">
+        The value that holds us true and to account
+      </h1>
 
-export default Value
+      <div className="grid gap-[10rem] grid-cols-3 items-center ">
+
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3 ">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center ">
+              <img src={twitter} alt="" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold ">
+            We believe in making free speach truly free for everyone, even by a little bit
+          </p>
+        </div>
+
+        <div className="singleGrid rounded-[10px] hover:bg-[#f7edf5] p-[1.5rem]">
+          <div className="flex items-center gap-3 ">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#f7d1e1] h-[40px] w-[40px] flex items-center justify-center ">
+              <img src={simple} alt="" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold ">
+            Things being made beautiful simple are at the heart of everything we
+            do
+          </p>
+        </div>
+
+        <div className="singleGrid rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]">
+          <div className="flex items-center gap-3 ">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#f3f2ad] h-[40px] w-[40px] flex items-center justify-center ">
+              <img src={tiktok} alt="" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold ">
+            We believe in making free speach truly free for everyone, even by a little bit
+          </p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Value;
